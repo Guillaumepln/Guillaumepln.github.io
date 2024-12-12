c8b56cbdaf8121c180396462491a1b634c71879a
Guillaumepln <74543855+Guillaumepln@users.noreply.github.com>
2024-12-12 11:26:41 +0100

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById('sideMenu');
-    if (menu.style.width === '250px') {
-        menu.style.width = '0';
-    } else {
-        menu.style.width = '250px';
-    }
-}
